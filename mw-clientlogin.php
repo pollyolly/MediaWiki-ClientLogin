afaf05d5335e4e08a5372e471cd3a74e53871c65
John Mark <johnmarkroco05@gmail.com>
2019-05-06 16:49:16 +0800

    Create mw-clientlogin.php

---
+++ mw-clientlogin.php
@@ -0,0 +1,49 @@
+error_reporting(E_ALL); ini_set('display_errors', 1);
+
+
+function getAuthToken(){
+
+        $cookie_file_path = tempnam('/tmp/tmp_cookie', 'isko_cookie'); //first create a directory in tmp folder
+        $url = ''; //sample.com/api.php?
+        $curlPost = array(
+                'action'=>'query',
+                'meta'=>'tokens',
+                'format'=>'json',
+                'type'=>'login'
+        );
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);        
+        curl_setopt($ch, CURLOPT_ENCODING, "UTF-8");    
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);            
+        curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_file_path);
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_file_path);            
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_COOKIESESSION, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($curlPost));
+        $data = json_decode(curl_exec($ch));
+        $curlPost2 = array(
+                'action'=>'clientlogin',
+                'loginreturnurl'=>'', //http://sample.com
+                'logintoken'=> trim($data->query->tokens->logintoken),
+                'format'=>'json',
+                'username'=>'', //account username
+                'password'=>'' //account password
+        );
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_ENCODING, "UTF-8");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_file_path);
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_file_path);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_COOKIESESSION, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($curlPost2));
+        $data2 = json_decode(curl_exec($ch));
+        curl_close($ch);
+        var_dump($data2);
+}
+
+getAuthToken();
